ef583ecdd7f5e148e28ac52789df352fcddf0aae
Mario9junior <mariojunior3251@gmail.com>
2026-05-04 02:24:38 -0300

    criando uma lista com os valores atualizadados
    criando uma lista com os valores atualizadados dos dados do asteroide em tempo de execução a cada novo valor a lista e atualizada dentro do for

--- asteroide.py
+++ asteroide.py
@@ -5,23 +5,27 @@ key_url = "1VMOiac1Qv0903cFqKB2xiID5zRmmZvl0sX4PJY9"
 data_from = "2020-09-07"
 date_to = "2020-09-08"
 url = f"https://api.nasa.gov/neo/rest/v1/feed?start_date={data_from}&end_date={date_to}&api_key={key_url}"
-print (url)
 
 response = requests.get(url)
 data_frame_aste = response.json()
 
+nome_asteroide = []
+potencialmente_perigoso =  []
+corpo_orbitando = []
+
+
 for date, asteroids in data_frame_aste["near_earth_objects"].items():
     for asteroid in asteroids:
-        print(f" - {asteroid['name']}: \n")
-        print(f" - {asteroid['is_potentially_hazardous_asteroid']}")
+       nome_asteroide.append(asteroid['name'])
+       potencialmente_perigoso.append(asteroid['is_potentially_hazardous_asteroid'])
 
 for date,objeto_planet in data_frame_aste['near_earth_objects'].items():
     for asteroide in objeto_planet:
         for close_approach in asteroide['close_approach_data']:
-            print(f" - {close_approach['orbiting_body']} \n")
+             corpo_orbitando = {close_approach['orbiting_body']}
       
- 
-
-
+print(f"O nome do asteroide : {nome_asteroide}")
+print(f"O asteroide é potencialmente perigoso :{potencialmente_perigoso}")
+print(f"O corpo que o asteroide está orbitando é: {corpo_orbitando}")
      
            
